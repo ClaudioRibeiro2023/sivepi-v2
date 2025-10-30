65889b47169ff846878e902758e4862240cfa227
Claudio Ribeiro <aeroeng.claudio@gmail.com>
2025-10-30 15:08:13 -0300

    🚀 ATIVAR v3.0 - SIVEPI Design System Moderno em PRODUÇÃO!

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './AppV3'; // ✅ v3.0 ATIVADO!
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-// Renderiza a aplicação
+// Renderiza a aplicação v3.0 - Design System Moderno
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
